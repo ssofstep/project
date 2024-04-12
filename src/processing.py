eee5640c0e452c837da9eabd431eaa14f3b33c88
sofia <example@mail.com>
2024-04-12 15:01:01 +0300

    fix mistakes and check linters

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_dict_by_state(change_users_list: list, state='EXECUTED') -> list:
+def filter_dict_by_state(change_users_list: list, state: str = "EXECUTED") -> list:
     """Функция, которая принимает список словарей и возвращает новый список, у которых ключ state"""
     new_users_list = []
     for i in change_users_list:
@@ -8,6 +8,7 @@ def filter_dict_by_state(change_users_list: list, state='EXECUTED') -> list:
     return new_users_list
 
 
-def sort_dict_by_date(list_with_data: list, reverse=True) -> list:
-    """Функция, которая принимает на вход список словарей и возвращает новый список, в котором исходные словари отсортированы по убыванию даты"""
+def sort_dict_by_date(list_with_data: list, reverse: bool = True) -> list:
+    """Функция, которая принимает на вход список словарей и возвращает новый список,
+    в котором исходные словари отсортированы по убыванию даты"""
     return sorted(list_with_data, key=lambda x: x["date"], reverse=reverse)
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,26 @@
+from src.processing import filter_dict_by_state, sort_dict_by_date
 from src.widget import data, masks_card_account
 
 print(masks_card_account("Maestro 1596837868705199"))
 print(masks_card_account("Счет 35383033474447895560"))
 print(data("2018-07-11T02:26:18.671407"))
+print(
+    filter_dict_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+print(
+    sort_dict_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
