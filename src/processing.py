14edbefbbe213e6889b74afed7d4d8fc166c3291
sofia <example@mail.com>
2024-04-12 14:44:43 +0300

    change function_name and add docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
-def new_sort_list(change_users_list: list, state='EXECUTED') -> list:
+def filter_dict_by_state(change_users_list: list, state='EXECUTED') -> list:
+    """Функция, которая принимает список словарей и возвращает новый список, у которых ключ state"""
     new_users_list = []
     for i in change_users_list:
         if i["state"] == state:
@@ -8,4 +9,5 @@ def new_sort_list(change_users_list: list, state='EXECUTED') -> list:
 
 
 def sort_dict_by_date(list_with_data: list, reverse=True) -> list:
+    """Функция, которая принимает на вход список словарей и возвращает новый список, в котором исходные словари отсортированы по убыванию даты"""
     return sorted(list_with_data, key=lambda x: x["date"], reverse=reverse)
