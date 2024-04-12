233d363f0ce02dd559373042330ae6f79a2f4e2e
sofia <example@mail.com>
2024-04-12 14:40:31 +0300

    add function sort_dict_by_date

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,7 @@ def new_sort_list(change_users_list: list, state='EXECUTED') -> list:
             new_users_list.append(i)
 
     return new_users_list
+
+
+def sort_dict_by_date(list_with_data: list, reverse=True) -> list:
+    return sorted(list_with_data, key=lambda x: x["date"], reverse=reverse)
