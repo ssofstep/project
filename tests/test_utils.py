f7b789f9b4505e14b30e2db645d158f48a348a53
sofia <example@mail.com>
2024-05-13 19:32:20 +0300

    done

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,41 @@
+import json
+import os
 from typing import Any
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
-import requests
+from dotenv import load_dotenv
 
 from src.utils import finance_transactions, sum_transactions
 
 
-def test_finance_transactions_true_path() -> None:
-    file_operations = "C:\\Users\\Student Free\\PycharmProjects\\project_skypro\\data\\operations.json"
-    assert {
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Maestro 1596837868705199",
-        "to": "Счет 64686473678894779589",
-    } == finance_transactions(file_operations)[0]
+@patch("builtins.open")
+def test_read_file(mock_open: Mock) -> None:
+    mock_file = mock_open.return_value.__enter__.return_value
+    mock_file.read.return_value = json.dumps(
+        [
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            }
+        ]
+    )
+    assert finance_transactions(os.path.join("..", "data", "operations.json")) == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
+        }
+    ]
+    mock_open.assert_called_once_with(os.path.join("..", "data", "operations.json"), "r", encoding="utf-8")
 
 
 def test_finance_transactions_false_path() -> None:
@@ -24,23 +43,54 @@ def test_finance_transactions_false_path() -> None:
     assert [] == finance_transactions(file_operations)
 
 
-def get_currency_info(currency: str) -> Any:
-    response = requests.get(f"https://v6.exchangerate-api.com/v6/e8d60e51fca9a55ec2011126/latest/{currency}")
-    return response.json()
+load_dotenv()
+api_token = os.getenv("API_KEY")
+headers = {"apikey": api_token}
+payload: dict[Any, Any] = {}
 
 
-def test_get_currency_info_usd() -> None:
-    with patch("requests.get") as mock_get:
-        mock_get.return_value.json.return_value = 751122.4502140001
-        assert get_currency_info("USD") == 751122.4502140001
-        mock_get.assert_called_once_with("https://v6.exchangerate-api.com/v6/e8d60e51fca9a55ec2011126/latest/USD")
+@patch("requests.get")
+def test_get_currency_info_usd(mock_get: Mock) -> None:
+    mock_get.return_value.json.return_value = {"conversion_rates": {"RUB": 52}}
+    assert (
+        sum_transactions(
+            {
+                "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019-07-03T18:35:29.512364",
+                "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+                "description": "Перевод организации",
+                "from": "MasterCard 7158300734726758",
+                "to": "Счет 35383033474447895560",
+            }
+        )
+        == 427511.24000000005
+    )
+    mock_get.assert_called_once_with(
+        f"https://v6.exchangerate-api.com/v6/{api_token}/latest/USD", headers=headers, data=payload
+    )
 
 
-def test_get_currency_info_eur() -> None:
-    with patch("requests.get") as mock_get:
-        mock_get.return_value.json.return_value = 808549.5418010001
-        assert get_currency_info("EUR") == 808549.5418010001
-        mock_get.assert_called_once_with("https://v6.exchangerate-api.com/v6/e8d60e51fca9a55ec2011126/latest/EUR")
+@patch("requests.get")
+def test_get_currency_info_eur(mock_get: Mock) -> None:
+    mock_get.return_value.json.return_value = {"conversion_rates": {"RUB": 12}}
+    assert (
+        sum_transactions(
+            {
+                "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019-07-03T18:35:29.512364",
+                "operationAmount": {"amount": "8221.37", "currency": {"name": "EUR", "code": "EUR"}},
+                "description": "Перевод организации",
+                "from": "MasterCard 7158300734726758",
+                "to": "Счет 35383033474447895560",
+            }
+        )
+        == 98656.44
+    )
+    mock_get.assert_called_once_with(
+        f"https://v6.exchangerate-api.com/v6/{api_token}/latest/EUR", headers=headers, data=payload
+    )
 
 
 def test_get_currency_info_rub() -> None:
