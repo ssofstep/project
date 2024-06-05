e2a6335d386fc5e98a45ec4b2f788301c33e9088
sofia <example@mail.com>
2024-06-05 19:59:01 +0300

    add something

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from src.utils import finance_transactions
+from src.utils import finance_transactions, read_csv_file, read_xlsx_file, sum_transactions
 from src.processing import filter_dict_by_state, sort_dict_by_date
+from src.widget import data, masks_card_account
 import os
-import re
 
-def main():
+from src.bank_operations import suitable_list
+
+
+def main() -> None:
+    "Функция сброки проекта"
     print('Привет! Добро пожаловать в программу работы с банковскими транзакициями. '
           'Выберите необходимый пункт меню: '
           '\n1. Получить информацию о транзакциях из json файла '
@@ -11,32 +15,79 @@ def main():
           '\n3. Получить информацию о транзакциях из xlsx файла')
     user_input = int(input())
     if user_input == 1:
-        while True:
-            print('Введите статус по которому необходимо выполнить фильтрацию. '
-                  '\nДоступные для фильтровки статусы: EXECUTED, CANCELED, PENDING')
-            user_status = input().upper()
-            if user_status in ["EXECUTED", "CANCELED", "PENDING"]:
-                list_operations = finance_transactions(os.path.join('data', 'operations.json'))
-                list_status = filter_dict_by_state(list_operations, user_status)
-                print("Отсортировать операции по дате? Да/Нет")
-                user_sort = input().lower()
-                if user_sort == "да":
-                    print("Отсортировать по возрастанию или по убыванию?")
-                    user_reverse = input().lower()
-                    if "возрастани" in user_reverse:
-                        list_sort_true = sort_dict_by_date(list_status, True)
-                        print("Выводить только рублевые тразакции? Да/Нет")
+        list_operations = finance_transactions(os.path.join('data', 'operations.json'))
+        print("Для обработки выбран json файл.")
+    elif user_input == 2:
+        list_operations = read_csv_file(os.path.join('data', 'transactions.csv'))
+        print("Для обработки выбран csv файл.")
+    elif user_input == 3:
+        list_operations = read_xlsx_file(os.path.join('data', 'transactions_excel.xlsx'))
+        print("Для обработки выбран xlsx файл.")
 
-                    elif "убывани" in user_reverse:
-                        list_sort_false = sort_dict_by_date(list_status, False)
-                        print("Выводить только рублевые тразакции? Да/Нет")
-                    # pattern = re.compile(r'возрастан')
-                    # user_reverse_pattern = re.findall(pattern, user_reverse)
-                elif user_sort == "нет":
+    while True:
+        print('Введите статус по которому необходимо выполнить фильтрацию. '
+              '\nДоступные для фильтровки статусы: EXECUTED, CANCELED, PENDING')
+        user_status = input().upper()
 
-                break
+        if user_status in ["EXECUTED", "CANCELED", "PENDING"]:
+            list_transactions = filter_dict_by_state(list_operations, user_status)
+
+            print("Отсортировать операции по дате? Да/Нет")
+
+            user_sort = input().lower()
+
+            if user_sort == "да":
+                print("Отсортировать по возрастанию или по убыванию?")
+
+                user_reverse = input().lower()
+
+                if "возрастани" in user_reverse:
+                    list_transactions = sort_dict_by_date(list_transactions)
+                elif "убывани" in user_reverse:
+                    list_transactions = sort_dict_by_date(list_transactions, False)
+
+            print("Выводить только рублевые тразакции? Да/Нет")
+
+            user_currency = input().lower()
+
+            if user_currency == "да":
+                list_currency = []
+
+                for i in list_transactions:
+                    if 'currency_code' in i.keys():
+                        if i['currency_code'] == "RUB":
+                            list_currency.append(i)
+                    else:
+                        for i in list_transactions:
+                            if i["operationAmount"]["currency"]["code"] == "RUB":
+                                list_currency.append(i)
             else:
-                print(f"Статус операции {user_status} недоступен.")
+                list_currency = list_transactions
+
+            print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
+
+            user_suitable = input().lower()
+            if user_suitable == "да":
+                print("Введите строку поиска в описании")
+                user_search_line = input().lower()
+                list_currency = suitable_list(list_currency, user_search_line)
+
+            print("Распечатываю итоговый список транзакций...")
+
+            if len(list_currency) != 0:
+                print(f'Всего банковских операций в выборке: {len(list_currency)}')
+                for i in list_currency:
+                    date = i["date"]
+                    description = i["description"]
+                    account_from = i["from"].get()
+                    account_to = i["to"]
+                    print(f'{data(date)} {description} \n{masks_card_account(account_from)} -> {masks_card_account()}')
+
+            else:
+                print("Не найдено ни одной транзакции подходящей под ваши условия фильтрации")
+            break
+        else:
+            print(f"Статус операции {user_status} недоступен.")
 
 
 if __name__ == '__main__':
--- src/bank_operations.py
+++ src/bank_operations.py
@@ -1,7 +1,8 @@
 import re
-
+from collections import Counter
 
 def suitable_list(list_operations: list[dict], search_line: str) -> list[dict]:
+    """Функция, которая возвращает список словарей, у которых в описании есть данная строка"""
     new_list = []
     pattern = re.compile(f"{search_line}")
     for i in list_operations:
@@ -11,3 +12,18 @@ def suitable_list(list_operations: list[dict], search_line: str) -> list[dict]:
     return new_list
 
 
+category = {"Перевод организации": "Перевод организации",
+            "Открытие вклада": "Открытие вклада",
+            "Перевод со счета на счет": "Перевод со счета на счет",
+            "Перевод с карты на счет": "Перевод с карты на счет"}
+
+
+def category_dict(list_operations: list[dict], category: dict) -> dict:
+    """Функция, которая возвращает словарь,где ключи — это названия категорий,
+    а значения — это количество операций в каждой категории"""
+    category_counts = Counter()
+    for i in list_operations:
+        item_category = i["description"]
+        if item_category in category:
+            category_counts[item_category] += 1
+    return category_counts
