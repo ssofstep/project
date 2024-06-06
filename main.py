dcd8414eede0fe31b3733b64c37b92acc5abf2dc
sofia <example@mail.com>
2024-06-06 18:03:13 +0300

    done

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-from src.utils import finance_transactions, read_csv_file, read_xlsx_file, sum_transactions
-from src.processing import filter_dict_by_state, sort_dict_by_date
-from src.widget import data, masks_card_account
 import os
 
 from src.bank_operations import suitable_list
+from src.processing import filter_dict_by_state, sort_dict_by_date
+from src.utils import finance_transactions, read_csv_file, read_xlsx_file
+from src.widget import data, masks_card_account
 
 
 def main() -> None:
     "Функция сброки проекта"
-    print('Привет! Добро пожаловать в программу работы с банковскими транзакициями. '
-          'Выберите необходимый пункт меню: '
-          '\n1. Получить информацию о транзакциях из json файла '
-          '\n2. Получить информацию о транзакциях из csv файла '
-          '\n3. Получить информацию о транзакциях из xlsx файла')
+    print(
+        "Привет! Добро пожаловать в программу работы с банковскими транзакициями. "
+        "Выберите необходимый пункт меню: "
+        "\n1. Получить информацию о транзакциях из json файла "
+        "\n2. Получить информацию о транзакциях из csv файла "
+        "\n3. Получить информацию о транзакциях из xlsx файла"
+    )
     user_input = int(input())
     if user_input == 1:
-        list_operations = finance_transactions(os.path.join('data', 'operations.json'))
+        list_operations = finance_transactions(os.path.join("data", "operations.json"))
         print("Для обработки выбран json файл.")
     elif user_input == 2:
-        list_operations = read_csv_file(os.path.join('data', 'transactions.csv'))
+        list_operations = read_csv_file(os.path.join("data", "transactions.csv"))
         print("Для обработки выбран csv файл.")
     elif user_input == 3:
-        list_operations = read_xlsx_file(os.path.join('data', 'transactions_excel.xlsx'))
+        list_operations = read_xlsx_file(os.path.join("data", "transactions_excel.xlsx"))
         print("Для обработки выбран xlsx файл.")
 
     while True:
-        print('Введите статус по которому необходимо выполнить фильтрацию. '
-              '\nДоступные для фильтровки статусы: EXECUTED, CANCELED, PENDING')
+        print(
+            "Введите статус по которому необходимо выполнить фильтрацию. "
+            "\nДоступные для фильтровки статусы: EXECUTED, CANCELED, PENDING"
+        )
         user_status = input().upper()
 
         if user_status in ["EXECUTED", "CANCELED", "PENDING"]:
@@ -54,8 +58,8 @@ def main() -> None:
                 list_currency = []
 
                 for i in list_transactions:
-                    if 'currency_code' in i.keys():
-                        if i['currency_code'] == "RUB":
+                    if "currency_code" in i.keys():
+                        if i["currency_code"] == "RUB":
                             list_currency.append(i)
                     else:
                         for i in list_transactions:
@@ -70,21 +74,28 @@ def main() -> None:
             user_suitable = input().lower()
             if user_suitable == "да":
                 print("Введите строку поиска в описании")
-                user_search_line = input().lower()
+                user_search_line = input().capitalize()
                 list_currency = suitable_list(list_currency, user_search_line)
                 print(list_currency)
             print("Распечатываю итоговый список транзакций...")
 
             if len(list_currency) != 0:
-                print(f'Всего банковских операций в выборке: {len(list_currency)}')
+                print(f"Всего банковских операций в выборке: {len(list_currency)}\n")
                 for i in list_currency:
                     date = i["date"]
                     description = i["description"]
-                    account_from = i.get("from", None)
+                    account_from = str(i.get("from", "without_from"))
                     account_to = i["to"]
-                    print(f'{data(date)} {description} ',
-                          f'\n{masks_card_account(account_from)} -> {masks_card_account(account_to)}',
-                          f'\nСумма: {sum_transactions(i)} руб.')
+                    if "amount" in i:
+                        amount = i["amount"]
+                    else:
+                        amount = i["operationAmount"]["amount"]
+
+                    print(
+                        f"{data(date)} {description} ",
+                        f"\n{masks_card_account(account_from)} -> {masks_card_account(account_to)}",
+                        f"\nСумма: {amount} руб.\n",
+                    )
 
             else:
                 print("Не найдено ни одной транзакции подходящей под ваши условия фильтрации")
@@ -93,5 +104,5 @@ def main() -> None:
             print(f"Статус операции {user_status} недоступен.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/bank_operations.py
+++ src/bank_operations.py
@@ -1,5 +1,7 @@
 import re
 from collections import Counter
+from typing import Dict
+
 
 def suitable_list(list_operations: list[dict], search_line: str) -> list[dict]:
     """Функция, которая возвращает список словарей, у которых в описании есть данная строка"""
@@ -12,16 +14,18 @@ def suitable_list(list_operations: list[dict], search_line: str) -> list[dict]:
     return new_list
 
 
-category = {"Перевод организации": "Перевод организации",
-            "Открытие вклада": "Открытие вклада",
-            "Перевод со счета на счет": "Перевод со счета на счет",
-            "Перевод с карты на счет": "Перевод с карты на счет"}
+category = {
+    "Перевод организации": "Перевод организации",
+    "Открытие вклада": "Открытие вклада",
+    "Перевод со счета на счет": "Перевод со счета на счет",
+    "Перевод с карты на счет": "Перевод с карты на счет",
+}
 
 
 def category_dict(list_operations: list[dict], category: dict) -> dict:
     """Функция, которая возвращает словарь,где ключи — это названия категорий,
     а значения — это количество операций в каждой категории"""
-    category_counts = Counter()
+    category_counts: Dict[str, int] = Counter()
     for i in list_operations:
         item_category = i["description"]
         if item_category in category:
